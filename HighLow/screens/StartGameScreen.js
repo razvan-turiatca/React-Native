436b54f21612078507b0f5aa78a0917d5c4f2a59
Razvan-Bogdan Turiatca <rturiatca@griddynamics.com>
2023-09-05 11:48:23 +0300

    start game screen ready to go

---
+++ HighLow/screens/StartGameScreen.js
@@ -0,0 +1,81 @@
+import { useState } from 'react';
+import { TextInput, View, StyleSheet, Alert } from 'react-native';
+import PrimaryButton from '../components/PrimaryButton';
+
+const StartGameScreen = () => {
+  const [enteredNumber, setEnteredNumber] = useState('');
+  function numberInputHandler(enteredText) {
+    setEnteredNumber(enteredText);
+  }
+  function resetInputHandler() {
+    setEnteredNumber('');
+  }
+
+  function confirmInputHandler() {
+    const chosenNumber = parseInt(enteredNumber);
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert('Invalid Number', 'Number has to be between 1 and 99', [
+        { text: 'Okay', style: 'destructive', onPress: resetInputHandler },
+      ]);
+      return;
+    }
+  }
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+        onChangeText={numberInputHandler}
+        value={enteredNumber}
+      />
+
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    backgroundColor: '#3b021f',
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 1,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+});
