0f63bf62be0f52153e50be9736a53c44ae3ddd0c
Razvan-Bogdan Turiatca <rturiatca@griddynamics.com>
2023-09-11 17:55:52 +0300

    almost done

--- FocusTimeApp/App.js
+++ FocusTimeApp/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   StyleSheet,
   Text,
@@ -6,13 +7,27 @@ import {
   Platform,
   StatusBar,
 } from 'react-native';
+
 import { colors } from './src/utils/colors';
 import Focus from './src/features/Focus';
+import { Countdown } from './src/components/CountDown';
+import { Timer } from './src/features/Timer';
 
 export default function App() {
+  const [currentSubject, setCurrentSubject] = useState(null);
   return (
     <SafeAreaView style={styles.container}>
-      <Focus />
+      {!currentSubject ? (
+        <Focus addSubject={setCurrentSubject} />
+      ) : (
+        <Timer
+          focusSubject={currentSubject}
+          onTimerEnd={() => {}}
+          clearSubject={() => {
+            setCurrentSubject(null);
+          }}
+        />
+      )}
     </SafeAreaView>
   );
 }
@@ -20,7 +35,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight + 20 : 0,
     backgroundColor: colors.darkBlue,
+  },
+  text: {
+    color: colors.white,
   },
 });
