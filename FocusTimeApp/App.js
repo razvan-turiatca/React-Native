5444ef8b7bfc06c7b045b703e5d1d63bb0790178
Razvan-Bogdan Turiatca <rturiatca@griddynamics.com>
2023-09-12 11:07:00 +0300

    finished history feature

--- FocusTimeApp/App.js
+++ FocusTimeApp/App.js
@@ -17,6 +17,7 @@ import { FocusHistory } from './src/features/FocusHistory';
 export default function App() {
   const [currentSubject, setCurrentSubject] = useState(null);
   const [history, setHistory] = useState([]);
+
   return (
     <SafeAreaView style={styles.container}>
       {!currentSubject ? (
@@ -27,7 +28,9 @@ export default function App() {
       ) : (
         <Timer
           focusSubject={currentSubject}
-          onTimerEnd={() => {}}
+          onTimerEnd={(subject) => {
+            setHistory([...history, subject]);
+          }}
           clearSubject={() => {
             setCurrentSubject(null);
           }}
